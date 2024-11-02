52c071e9b9b4ec0b90b2a8231a46aab2b32e7b23
SakshiBishnoi <sakshivishnoi5720@gmail.com>
2024-11-02 06:03:18 +0530

    Fix 7 cors

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from '../utils/token';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://taskmanagerapp-sakshibishnoi.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
